f88427790aac65dabb9e6e69925f613f17071f3e
Freja Payne <Freja.payne@elev.ga.ntig.se>
2023-09-01 10:05:30 +0200

    jbliub

---
+++ Bash-a-monster/Program.cs
@@ -0,0 +1,15 @@
+﻿using BashMonster;
+using Namespace;
+
+Random generator = new Random();
+int r = generator.Next(20);
+
+Gubbe player = new();
+Enemy enemy = new();
+
+while (player.hp > 0 && enemy.hp > 0)
+{
+    Console.WriteLine($"Player: {GubbeHp}  Freja: {ch2Hp}\n"")
+}
+
+Console.ReadLine();
